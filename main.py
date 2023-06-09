80cb080c502624b773ac1fe48743c18744ae1b6f
smenp <126504818+smenp@users.noreply.github.com>
2023-06-09 11:15:19 +0200

    Added tabs on main page

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import repo
 
-st.write("Enter probe name")
-probe_name = st.text_input("Probe name")
-probe_num = st.number_input("Probe number", 1, step=1)
-submitted = st.button("Submit")
+view_tab, add_tab = st.tabs(["View Probes", "Add Probe"])
 
-if submitted:
-    repo.add(probe_name, probe_num)
+with add_tab:
+    st.write("Enter probe name")
+    probe_name = st.text_input("Probe name")
+    probe_num = st.number_input("Probe number", 1, step=1)
+    submitted = st.button("Submit")
 
-get_probes = st.button("List all probes")
-if get_probes:
+    if submitted:
+        repo.add(probe_name, probe_num)
+
+with view_tab:
     probes_df = repo.get_all_probes()
     st.dataframe(probes_df)
