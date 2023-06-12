b73a0f89a8dd48a239144d7c47dd0cf72da7e508
smenp <smarguet@uni-koeln.de>
2023-06-12 16:58:38 +0200

    Implemented SqliteRepo

---
+++ sqlite_repo.py
@@ -0,0 +1,45 @@
+import os
+import sqlite3
+
+import pandas as pd
+
+from repo_abc import IProbeRepo
+
+# library portion
+class SqliteRepo(IProbeRepo):
+    def __init__(self, sqlite_file: str):
+        self.sqlite_file = sqlite_file
+        self.conn = sqlite3.connect(sqlite_file)
+        self.conn.execute(f"""
+            CREATE TABLE probes(
+                name VARCHAR,
+                number INTEGER
+            )""")
+
+
+    def add(self, name, number):
+        self.conn.execute(f"""
+            INSERT INTO probes(name, number)
+            VALUES ("{name}", "{number}")
+        """)
+    
+    def get_all_probes(self) -> pd.DataFrame:
+        probe_column_names = \
+            [row[1] for row in self.conn.execute(\
+                "PRAGMA table_info(probes)").fetchall()]
+            
+        data = self.conn.execute("SELECT * FROM probes").fetchall()
+        
+        return pd.DataFrame(data, columns=probe_column_names)
+        
+    def delete(self, probe_ids):
+        ...
+
+# script portion --part run from command line
+if __name__ == "__main__":
+    sqlite_file = 'probes.sqlite'
+    os.remove(sqlite_file)
+    repo = SqliteRepo(sqlite_file)
+    repo.add("probe1",1)
+    print(repo.get_all_probes())
+    
